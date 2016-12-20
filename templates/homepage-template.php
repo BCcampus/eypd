a7a1c2176e302e51c8c9acdcccbcab2d3184fae7
bdolor <bpayne@bccampus.ca>
2016-12-19 16:13:54 -0800

    fixing homepage footer inconsistency

--- templates/homepage-template.php
+++ templates/homepage-template.php
@@ -22,10 +22,6 @@ infinity_get_header();
 </div>
 
 <div class="row">
-	<?php
-	do_action( 'open_content' );
-	do_action( 'open_page' );
-	?>
     <div class="c-search">
         <h2 class="text-blue text-center">Search for training events</h2>
         <p class="text-center">Fill in one or more of the fields below</p>
@@ -36,29 +32,33 @@ infinity_get_header();
 <div class="c-map row">
     <h2 class="text-blue text-center">Find training events near you</h2>
     <div class="four columns">
-      <h3>Upcoming Events</h3>
-      	<?php
-      	// $events_list = '[events_list scope="future" limit="6"]<p>#_EVENTLINK will take place at #_LOCATIONLINK on #_EVENTDATES</p>[/events_list]';
-      	$events_list = '[events_list scope="future" limit="3"]';
-      	echo do_shortcode( $events_list );
-      	?>
+        <h3>Upcoming Events</h3>
+		<?php
+		$events_list = '[events_list scope="future" limit="3"]';
+		echo do_shortcode( $events_list );
+		?>
     </div>
     <div class="twelve columns">
-    	<?php
-    	infinity_load_template( 'templates/google-map.php' );
-    	?>
+		<?php
+		infinity_load_template( 'templates/google-map.php' );
+		?>
     </div>
 </div>
+<div id="content" role="main" class="column sixteen">
+	<?php
+	do_action( 'open_content' );
+	do_action( 'open_home' );
+	?>
 
-<div id="content">
-        <h2 class="text-blue text-center">Explore the professional learning showcase</h2>
-		<?php infinity_load_template( 'templates/featured-stories.php' ); ?>
+    <h2 class="text-blue text-center">Explore the professional learning showcase</h2>
+	<?php infinity_load_template( 'templates/featured-stories.php' ); ?>
+
+	<?php
+	do_action( 'close_home' );
+	do_action( 'close_content' );
+
+	?>
 </div>
-
-<?php
-do_action( 'close_page' );
-do_action( 'close_content' );
-?>
 <?php
 infinity_get_footer();
 ?>
