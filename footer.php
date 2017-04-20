c29802025dde3a51329824625ed990aad951a89f
alexpflores <alex.paredes.flores@gmail.com>
2017-04-20 12:51:39 -0700

    Update footer to match eypd template files

--- footer.php
+++ footer.php
@@ -1,14 +1,16 @@
 <?php
 /**
- * Infinity Theme: footer template
+ * Early-Years Theme: footer template
  *
+ * Modified from original header template in cbox theme
+ * @author Brad Payne
+ * @package early-years
+ * @since 0.9
+ * @license https://www.gnu.org/licenses/gpl.html GPLv3 or later
+ *
+ * Original:
  * @author Bowe Frankema <bowe@presscrew.com>
- * @link http://infinity.presscrew.com/
  * @copyright Copyright (C) 2010-2011 Bowe Frankema
- * @license http://www.gnu.org/licenses/gpl.html GPLv2 or later
- * @package Infinity
- * @subpackage templates
- * @since 1.0
  */
 ?>
 
