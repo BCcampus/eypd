254f5e63f0e59863b46add4eedbbf8843a231f43
alexpflores <alex.paredes.flores@gmail.com>
2017-04-25 15:23:17 -0700

    update version number

--- functions.php
+++ functions.php
@@ -192,7 +192,7 @@ function eypd_get_provinces() {
 function eypd_run_once() {
 
 	// change eypd_version value to run it again
-	$eypd_version        = 26;
+	$eypd_version        = 5.2;
 	$current_version     = get_option( 'eypd_version', 0 );
 	$img_max_dimension   = 1000;
 	$img_min_dimension   = 50;
