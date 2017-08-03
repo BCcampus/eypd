e9409b706102331c7f07f64137cc2a29be749ff5
alexpflores <alex.paredes.flores@gmail.com>
2017-08-03 14:55:42 -0700

    Revert "Clarification and Notification Sprint (#208)" (#209)
    This reverts commit f796b659c7fa1bc3dc0214aa0367fbe23c1f2a88.

--- plugins/events-manager/forms/event-editor.php
+++ plugins/events-manager/forms/event-editor.php
@@ -37,7 +37,7 @@ if ( ! empty( $_REQUEST['success'] ) ) {
 	}
 }
 
-if ( empty ( $_REQUEST['success'] ) ) {
+if ( ! $_REQUEST['success'] ) {
 	echo "<p class='margin-up'>";
 	_e( "This form is to be completed in order to post events on this web portal. To add your event, fill in the fields below. Fields marked with an * are mandatory. If you have multiple events to post please contact earlyyears[at]bccampus.ca for assistance.", 'early-years' );
 	echo "</p>";
