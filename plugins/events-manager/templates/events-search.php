57ce0ef2f4c3d5ac56d41b2079535f36778b9ded
bdolor <bpayne@bccampus.ca>
2017-11-16 11:02:55 -0800

    adding doc block

--- plugins/events-manager/templates/events-search.php
+++ plugins/events-manager/templates/events-search.php
@@ -1,12 +1,17 @@
 <?php
-/* WARNING!!! (2013-07-10) We intend to add a few more fields into this search form over the coming weeks/months. 
- * Overriding shouldn't hinder functionality at all but these new search options won't appear on your form! 
+/**
+ * Modified from original events manager plugin version: 5.7.3
+ *
+ * @author Alex Paredes
+ * @package early-years
+ * @since 0.9.6
+ * @license https://www.gnu.org/licenses/gpl.html GPLv3 or later
+ *
+ * Original:
+ * @author Marcus Sykes
+ * @copyright Copyright Marcus Sykes
  */
-/* 
- * By modifying this in your theme folder within plugins/events-manager/templates/events-search.php, you can change the way the search form will look.
- * To ensure compatability, it is recommended you maintain class, id and form name attributes, unless you now what you're doing. 
- * You also have an $args array available to you with search options passed on by your EM settings or shortcode
- */
+
 $args = ! empty( $args ) ? $args : array(); /* @var $args array */
 ?>
 <div class="em-search-wrapper">
