7cd0f1b73046a3616e1cb54ba92c89621f463b25
alex-418 <aparedes@bccampus.ca>
2018-05-31 14:53:05 -0700

    rm accordion and arrows from future events list, as per new design in issue 450

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,8 +1,5 @@
 (function ($) {
     $(document).ready(function () {
-        $('#future').click(function () {
-            $(this).find('i').toggleClass('glyphicon-triangle-right').toggleClass('glyphicon-triangle-bottom');
-        });
         $('#past').click(function () {
             $(this).html() === 'Collapse past events <i class="glyphicon glyphicon-triangle-bottom"></i>' ? $(this).html('Expand to see all past events <i class="glyphicon glyphicon-triangle-right"></i>') : $(this).html('Collapse past events <i class="glyphicon glyphicon-triangle-bottom"></i>');
         });
