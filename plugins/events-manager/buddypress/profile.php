7dd775457da9472ae802258af229549e6fd42548
alex-418 <aparedes@bccampus.ca>
2018-06-01 10:33:10 -0700

    donut placeholder image if no data

--- plugins/events-manager/buddypress/profile.php
+++ plugins/events-manager/buddypress/profile.php
@@ -112,14 +112,15 @@ if ( user_can( $bp->displayed_user->id, 'edit_events' ) ) {
 |
 */
 if ( bp_is_my_profile() ) {
-	$cert_hours       = get_user_meta( $bp->displayed_user->id, 'eypd_cert_hours', true );
+	$cert_hours       = get_user_meta( $bp->displayed_user->id, 'eypd_cert_hours', TRUE );
 	$chart_data_array = eypd_hours_and_categories( $cert_hours );
 	$chart_data_json  = eypd_d3_array( $chart_data_array );
 
 	// Pass the data to donut.js
 	wp_localize_script( 'donut', 'donut_data', $chart_data_json );
+	$no_donut = get_stylesheet_directory_uri() . '/dist/images/donut_placeholder.png';
 	echo '<h2 class="top-padding">Event summary</h2>';
-	echo '<div class="donut"></div>';
+	echo ( $chart_data_json ) ? '<div class="donut"></div>' : "<div class='no-donut'><img src={$no_donut}></div>";
 }
 
 /*
