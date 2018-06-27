14109297bc28f822e21f94b697755a8330004eb6
alex-418 <aparedes@bccampus.ca>
2018-06-27 12:41:53 -0700

    update description text

--- plugins/events-manager/buddypress/profile.php
+++ plugins/events-manager/buddypress/profile.php
@@ -17,7 +17,7 @@ global $bp, $EM_Notices;
 echo $EM_Notices;
 
 echo "<div class='row'><p class='desc col-md-8'>This is your professional development activity page - a personal record of your training events, events you plan on 
-attending, and record of professional development hours you have accumulated.</p></div>";
+attending, and the professional development hours you have accumulated.</p></div>";
 
 /*
 |--------------------------------------------------------------------------
