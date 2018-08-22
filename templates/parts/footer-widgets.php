5c67857c0c8cf3735d9aab64da38d24fda474bd8
alex-418 <aparedes@bccampus.ca>
2018-08-22 15:11:56 -0700

    set mobile svg image size

--- templates/parts/footer-widgets.php
+++ templates/parts/footer-widgets.php
@@ -54,7 +54,7 @@
 		<span class="fb"><a target=_blank href="https://www.facebook.com/BCEYPD/"><img height="35px" width="35px" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icon-facebook.svg"></a></span>
 	</div>
 	<div class="ten columns hidden-md hidden-lg">
-		<span class="fb"><a target=_blank href="https://www.facebook.com/BCEYPD/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icon-facebook.svg"></a></span>
+		<span class="fb"><a target=_blank href="https://www.facebook.com/BCEYPD/"><img height="35px" width="35px" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/icon-facebook.svg"></a></span>
 	</div>
 
 	<div class="widget six columns">
