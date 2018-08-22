b218ba159fc834807c0557facc8ef52c6fba71cb
alex-418 <aparedes@bccampus.ca>
2018-08-22 11:38:28 -0700

    add open graph tags to pages

--- functions.php
+++ functions.php
@@ -921,17 +921,25 @@ function eypd_fb_opengraph() {
 			$excerpt = get_bloginfo( 'description' );
 		}
 		?>
-
 		<meta property="og:title" content="<?php echo the_title(); ?>"/>
 		<meta property="og:description" content="<?php echo $excerpt; ?>"/>
 		<meta property="og:type" content="article"/>
 		<meta property="og:url" content="<?php echo the_permalink(); ?>"/>
 		<meta property="og:site_name" content="<?php echo get_bloginfo( 'name' ); ?>"/>
 		<meta property="og:image" content="<?php echo $img_src; ?>"/>
-
 		<?php
-	} else {
-		?>
+		if ( is_page() ) {
+			if ( has_post_thumbnail( $post->ID ) ) {
+				$img_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+			} ?>
+			<meta property="og:title" content="<?php echo the_title(); ?>"/>
+			<meta property="og:description" content="<?php echo get_the_title(); ?>"/>
+			<meta property="og:type" content="page"/>
+			<meta property="og:url" content="<?php echo the_permalink(); ?>"/>
+			<meta property="og:site_name" content="<?php echo get_bloginfo( 'name' ); ?>"/>
+			<meta property="og:image" content="<?php echo $img_src; ?>"/>
+		<?php }
+	} else { ?>
 		<meta property="og:title" content="<?php echo get_bloginfo( 'name' ); ?>"/>
 		<meta property="og:description" content="<?php echo get_bloginfo( 'description' ); ?>"/>
 		<meta property="og:type" content="website"/>
