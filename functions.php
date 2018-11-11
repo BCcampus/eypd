bb4b73f0b3a73d8e25cc19966ee63217204baedd
Brett Forsyth <info@brettforsyth.com>
2018-11-11 10:08:04 -0500

    Update functions.php
    Added spacing to rectify build errors

--- functions.php
+++ functions.php
@@ -128,8 +128,8 @@ add_action(
 		wp_enqueue_script( 'events-manager', $template_dir . '/dist/scripts/events-manager.js', array_values( $script_deps ), isset( $EM_VERSION ) );
 		wp_enqueue_script( 'tinyscrollbar', $template_dir . '/dist/scripts/jquery.tinyscrollbar.min.js', [ 'jquery' ], '1.0', true );
 
-		wp_enqueue_script( 'popper', $template_dir . '/dist/scripts/popper.min.js', ['jquery'], null, true );
-		wp_enqueue_script( 'bootstrap-script', $template_dir . '/dist/scripts/bootstrap.min.js', ['popper'], null, true );
+		wp_enqueue_script( 'popper', $template_dir . '/dist/scripts/popper.min.js', [ 'jquery' ], null, true );
+		wp_enqueue_script( 'bootstrap-script', $template_dir . '/dist/scripts/bootstrap.min.js', [ 'popper' ], null, true );
 		wp_enqueue_style( 'bootstrap-style', $template_dir . '/dist/styles/bootstrap.min.css' );
 
 		// load popover only for users who aren't logged in
@@ -139,8 +139,7 @@ add_action(
 			wp_enqueue_script( 'initpopover', $template_dir . '/dist/scripts/initpopover.js', [ 'bootstrap-popover' ], null, true );
 			wp_enqueue_script( 'popover-dismiss', $template_dir . '/dist/scripts/popover-dismiss.js', [ 'initpopover' ], null, true );
 		}
-
-
+		
 		wp_enqueue_script( 'modal-video', $template_dir . '/dist/scripts/modal-video.js', [ 'jquery' ], null, true );
 
 		// load styling for datepicker in myEYPD profile page only
