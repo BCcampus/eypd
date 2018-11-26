03b854bbe9047a96b1348a6f7d2dd2c938486822
Brett Forsyth <info@brettforsyth.com>
2018-11-26 10:26:46 -0500

    phpcbf corrections

--- members/single/home.php
+++ members/single/home.php
@@ -36,33 +36,33 @@ get_header( 'buddypress' ); ?>
 
 				<?php do_action( 'bp_before_member_body' );
 
-				if ( bp_is_user_activity() || !bp_current_component() ) :
-					locate_template( array( 'members/single/activity.php'  ), true );
+				if ( bp_is_user_activity() || ! bp_current_component() ) :
+					locate_template( array( 'members/single/activity.php' ), true );
 
 				 elseif ( bp_is_user_blogs() ) :
-					locate_template( array( 'members/single/blogs.php'     ), true );
+						locate_template( array( 'members/single/blogs.php' ), true );
 
 				elseif ( bp_is_user_friends() ) :
-					locate_template( array( 'members/single/friends.php'   ), true );
+					locate_template( array( 'members/single/friends.php' ), true );
 
 				elseif ( bp_is_user_groups() ) :
-					locate_template( array( 'members/single/groups.php'    ), true );
+					locate_template( array( 'members/single/groups.php' ), true );
 
 				elseif ( bp_is_user_messages() ) :
-					locate_template( array( 'members/single/messages.php'  ), true );
+					locate_template( array( 'members/single/messages.php' ), true );
 
 				elseif ( bp_is_user_profile() ) :
-					locate_template( array( 'members/single/profile.php'   ), true );
+					locate_template( array( 'members/single/profile.php' ), true );
 
 				elseif ( bp_is_user_settings() ) :
-					locate_template( array( 'members/single/settings.php'  ), true );
+					locate_template( array( 'members/single/settings.php' ), true );
 
 				elseif ( function_exists( 'bp_is_user_notifications' ) && bp_is_user_notifications() ) :
 					locate_template( array( 'members/single/notifications.php' ), true );
 
-				// If nothing sticks, load a generic template
+					// If nothing sticks, load a generic template
 				else :
-					locate_template( array( 'members/single/plugins.php'   ), true );
+					locate_template( array( 'members/single/plugins.php' ), true );
 
 				endif;
 
--- members/single/profile/profile-loop.php
+++ members/single/profile/profile-loop.php
@@ -1,19 +1,19 @@
 <?php do_action( 'bp_before_profile_loop_content' ); ?>
-<?php $role =  bp_get_profile_field_data("field=Position/Role"); ?>
+<?php $role = bp_get_profile_field_data( 'field=Position/Role' ); ?>
 
 
 <?php if ( bp_has_profile() ) : ?>
 
-	<h2><?php echo bp_profile_field_data("field=First and Last Name"); ?></h2>
+	<h2><?php echo bp_profile_field_data( 'field=First and Last Name' ); ?></h2>
 	<p><strong><?php echo $role; ?></strong><br>
 		<?php
-		if( $role === "Organizer" ){
-			echo 'Agency/Institution: <a href="/?members_search.php=' . urlencode(bp_get_profile_field_data("field=Agency/Institution (Select if you are an Organizer of training)")) . '">' . bp_get_profile_field_data("field=Agency/Institution (Select if you are an Organizer of training)") . '</a>';
+		if ( $role === 'Organizer' ) {
+			echo 'Agency/Institution: <a href="/?members_search.php=' . urlencode( bp_get_profile_field_data( 'field=Agency/Institution (Select if you are an Organizer of training)' ) ) . '">' . bp_get_profile_field_data( 'field=Agency/Institution (Select if you are an Organizer of training)' ) . '</a>';
 		}
 		?>
 	</p>
-	<p><?php bp_profile_field_data("field=Name of your place of Work"); ?><br>
-		<?php echo bp_get_profile_field_data("field=City/Town")  . ", " . bp_get_profile_field_data("field=Province"); ?>
+	<p><?php bp_profile_field_data( 'field=Name of your place of Work' ); ?><br>
+		<?php echo bp_get_profile_field_data( 'field=City/Town' ) . ', ' . bp_get_profile_field_data( 'field=Province' ); ?>
 	</p>
 	<?php /* while ( bp_profile_groups() ) : bp_the_profile_group(); ?>
 
