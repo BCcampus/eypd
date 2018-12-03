0f04e23eca7af3ee9022a76247ca937932f4db03
Brett Forsyth <info@brettforsyth.com>
2018-12-03 11:38:03 -0500

    Limitied activity page
    Display only the user name on the activity page for everyone except the activity pages owner. No xprofile data is available.

--- members/single/activity.php
+++ members/single/activity.php
@@ -79,29 +79,13 @@ do_action( 'bp_before_member_activity_content' ); ?>
 <?php do_action( 'bp_after_member_activity_content' ); ?>
 
 <?php
+	//Limit the user profile to just their name and address when not themselves
 	else :
 ?>
-
 <?php do_action( 'bp_before_profile_loop_content' ); ?>
 
-<?php $role = bp_get_profile_field_data( 'field=Position/Role' ); ?>
-
-<?php if ( bp_has_profile() ) : ?>
-	<h2><?php echo bp_profile_field_data( 'field=First and Last Name' ); ?></h2>
-	<p><strong><?php echo $role; ?></strong><br>
-		<?php
-		if ( $role === 'Organizer' ) {
-			echo 'Agency/Institution: <a href="/?members_search.php=' . urlencode( bp_get_profile_field_data( 'field=Agency/Institution (Select if you are an Organizer of training)' ) ) . '">' . bp_get_profile_field_data( 'field=Agency/Institution (Select if you are an Organizer of training)' ) . '</a>';
-		}
-		?>
-	</p>
-	<p><?php bp_profile_field_data( 'field=Name of your place of Work' ); ?><br>
-		<?php echo bp_get_profile_field_data( 'field=City/Town' ) . ', ' . bp_get_profile_field_data( 'field=Province' ); ?>
-	</p>
-<?php do_action( 'bp_profile_field_buttons' ); ?>
-
-<?php endif; ?>
-
+<?php //Just show the user name when not logged in. No other xprofile fields are available ?>
+<h2><?php echo bp_profile_field_data( 'field=First and Last Name' ); ?></h2>
 <?php do_action( 'bp_after_profile_loop_content' ); ?>
 
 <?php
