62e69d54263229f9aca134e2910dd9065b759a10
alex-418 <aparedes@bccampus.ca>
2019-05-22 17:17:33 -0700

    let the spaceship operator sort donut data by decesnding value

--- functions.php
+++ functions.php
@@ -1182,8 +1182,12 @@ function eypd_d3_array( $data ) {
 		}
 	}
 
-	return $result;
+	$sorter = function( $a, $b ) { return $b <=> $a;
+	};
+	usort( $result, $sorter );
+	print_r( $result );
 
+	return $result;
 }
 
 /**
